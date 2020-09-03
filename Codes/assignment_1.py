afc54e3563633cb2de21d2bdd389c2121a8923c2
Vaibhav11002 <70570778+Vaibhav11002@users.noreply.github.com>
2020-09-03 19:36:03 +0530

    Update assignment_1.py

--- Codes/assignment_1.py
+++ Codes/assignment_1.py
@@ -4,10 +4,10 @@ k = 8
 a = [2,2,3]
 b = [-1,2,1]
 c= [3,1,0]
-d= [2-k , 2+2*k , 3+k]
+d= [2-k , 2+2*k , 3+k]   #d=(a+kb)
 
 ans = np.dot(c,d)
 
-
+#dot product is zero if the two vectors are perpendicular
 if ans == 0 :
   print("The vectors are perpendicular for value of k =", 8)
