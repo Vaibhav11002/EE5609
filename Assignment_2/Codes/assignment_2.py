5def5318c4fd0fa73e35a867a9552acbf603d356
Vaibhav11002 <70570778+Vaibhav11002@users.noreply.github.com>
2020-09-07 22:14:52 +0530

    Update assignment_2.py

--- Assignment_2/Codes/assignment_2.py
+++ Assignment_2/Codes/assignment_2.py
@@ -1,13 +1,12 @@
 import numpy as np
 
-y = np.array([[3,2], [1,4]])
+Y = np.array([[3,2], [1,4]])
 
 #initialising the value of X to verify the problem
 
 X = np.array([[-1,-1],[-2,-1]])
 
 #calculating the matrix 2X+Y 
+ans = 2*X + Y
 
-2XplusY = 2*X + Y
-
-print("2X + Y = ", 2XplusY)
+print("2X + Y = ", ans)
